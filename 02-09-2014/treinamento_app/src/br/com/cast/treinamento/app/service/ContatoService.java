13ea68c3787d9af307083b9c35b40b2077a8759d
Felipe Coelho <felipeisrael@gmail.com>
2014-09-02 20:26:41 -0300

    Nosso segundo dia

---
+++ 02-09-2014/treinamento_app/src/br/com/cast/treinamento/app/service/ContatoService.java
@@ -0,0 +1,28 @@
+package br.com.cast.treinamento.app.service;
+
+import java.util.List;
+
+import br.com.cast.treinamento.app.dao.ContatoDao;
+import br.com.cast.treinamento.app.entite.Contato;
+
+public final class ContatoService {
+
+	private static final ContatoService INSTANCIA = new ContatoService();
+
+	private ContatoService() {
+		super();
+	}
+
+	public static ContatoService getInstancia() {
+		return INSTANCIA;
+	}
+
+	public List<Contato> listarTodos() {
+		return ContatoDao.getInstancia().listarTodos();
+	}
+
+	public void salvar(Contato contato) {
+		ContatoDao.getInstancia().salvar(contato);
+	}
+
+}
